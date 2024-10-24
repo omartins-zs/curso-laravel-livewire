49e550e6f1e8ca21145bbbf67e6eeb42c3d00b36
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-24 16:16:53 -0300

    Criando Rota do Exemplo Counter Tutorial Livewire

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Counter;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Exemplo Counter Tutorial Livewire Documentação
+Route::get('/counter', function () {
+    return view('counter-page');
 });
