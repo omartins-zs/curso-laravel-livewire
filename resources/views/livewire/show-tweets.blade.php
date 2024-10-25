c70015af0b49a6eb75ba43fc638c373cdea67975
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-25 15:17:29 -0300

    Adicioanando Formulario para Criar Tweet

--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -2,7 +2,13 @@
     <h1>Show Tweets</h1>
 
     <p>{{ $message }}</p>
-    <input type="text" name="message" id="message" wire:model.live='message'>
+
+    <form method="post" wire:submit.prevent='create'>
+
+        <input type="text" name="message" id="message" wire:model.live='message'>
+
+        <button type="submit">Criar Tweet</button>
+    </form>
 
     <hr>
 
