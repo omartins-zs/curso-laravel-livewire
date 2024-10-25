9094a96d1924b0c430095aef5131064d53ea7768
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-25 16:37:53 -0300

    Add Tweet com User Autenticado e Ajuste Paginate

--- app/Livewire/ShowTweets.php
+++ app/Livewire/ShowTweets.php
@@ -18,7 +18,7 @@ class ShowTweets extends Component
 
     public function render()
     {
-        $tweets = Tweet::with('user')->paginate(2);
+        $tweets = Tweet::with('user')->latest()->paginate(2);
 
         return view('livewire.show-tweets', compact('tweets'));
 
@@ -36,10 +36,14 @@ class ShowTweets extends Component
 
         $this->validate();
 
-        Tweet::create([
+        auth()->user()->tweets()->create([
             'content' => $this->content,
-            'user_id' => 1,
         ]);
+
+        // Tweet::create([
+        //     'content' => $this->content,
+        //     'user_id' => auth()->user()->id,
+        // ]);
 
         $this->content = '';
     }
