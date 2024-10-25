d34e15796e7759712a647673af3a5197fb3f4382
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-25 11:12:15 -0300

    Add message e mudança de var com Databind Livewire

--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,3 +1,6 @@
 <div>
-    {{-- Do your work, then step back. --}}
+    <h1>Show Tweets</h1>
+
+    <p>{{ $message }}</p>
+    <input type="text" name="message" id="message" wire:model.live='message'>
 </div>
