d77a186fc4f5cf2dab8d46a0edc6e1522c5360a6
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-25 11:09:33 -0300

    Add Message no Component

--- app/Livewire/ShowTweets.php
+++ app/Livewire/ShowTweets.php
@@ -6,8 +6,12 @@ use Livewire\Component;
 
 class ShowTweets extends Component
 {
+    public $message = "Apenas um teste";
+
     public function render()
     {
         return view('livewire.show-tweets');
+        // Se não mudar o Layout padrão do Livewire usar a linha abaixo
+        // return view('livewire.show-tweets')->layout('layouts.app'); // Use a referência correta
     }
 }
