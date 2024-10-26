2f8058346b2d671713e9acb55399fe29e548f07c
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-26 09:28:22 -0300

    Add prop photo e Criando function storagePhoto

--- app/Livewire/User/UploadPhoto.php
+++ app/Livewire/User/UploadPhoto.php
@@ -6,8 +6,15 @@ use Livewire\Component;
 
 class UploadPhoto extends Component
 {
+    public $photo;
+
     public function render()
     {
         return view('livewire.user.upload-photo');
+    }
+
+    public function storagePhoto()
+    {
+        dd("CXheugue");
     }
 }
