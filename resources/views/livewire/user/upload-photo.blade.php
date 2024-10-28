f863377cae50066de80ba17a456810fa6d42cf14
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-28 09:41:08 -0300

    Personalizando Layout e Add Preview da Imagem

--- resources/views/livewire/user/upload-photo.blade.php
+++ resources/views/livewire/user/upload-photo.blade.php
@@ -1,13 +1,19 @@
 <div>
+    <h1 class="text-4xl py-6 font-bold">Atualizar Foto do Perfil</h1>
+    <form wire:submit.prevent="storagePhoto" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8 m-8">
+        @if ($photo)
+            <div class="mb-4">
+                <img src="{{ $photo->temporaryUrl() }}" style="max-width: 200px;">
+            </div>
+        @endif
+        <div class="mb-4">
+            <input type="file" wire:model="photo">
+        </div>
 
-    <h1>Atualizar foto de perfil</h1>
-    <form action="" method="post" wire:submit.prevent='storagePhoto'>
-
-        <input type="file" wire:model="photo">
         @error('photo')
-            {{ $message }}
+            <p><span class="text-red-500">{{ $message }}</span></p>
         @enderror
 
-        <button type="submit">Upload foto</button>
+        <button type="submit" class="bg-blue-900 text-white p-2 pl-6 pr-6 rounded">Upload Foto</button>
     </form>
 </div>
